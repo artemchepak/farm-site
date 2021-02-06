0e980d71716e8fc584a71d5da1195962679559b3
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2021-02-06 22:17:24 +0200

    feat: add products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 $(function() {
+    //top slider 
+
     $('.top__inner').slick({
         arrows: false,
         dots: true,
         autoplay: true,
         autoplaySpeed: 2000
+    });
+
+    //products__card-btn   
+    $('.products__card-btn').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('products__card-btn--active');
     });
 });
